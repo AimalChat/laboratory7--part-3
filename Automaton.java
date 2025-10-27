584c5174bbf8b0ad4ef85cbb61bfa44a8d7cece3
AimalChat <6318359@edu.vaniercollege.qc.ca>
2025-10-26 21:10:55 -0400

    FINISHED! :D

--- Automaton.java
+++ Automaton.java
@@ -41,6 +41,13 @@ public class Automaton
         System.out.println();
     }   
     
+    public int calculateNextState35(int left, int center, int right)
+    {
+        int nextState;
+        nextState = (center + right) % 2;
+        return nextState;
+    }
+    
     public int calculateNextState(int left, int center, int right)
     {
         int nextState;
@@ -48,11 +55,51 @@ public class Automaton
         return nextState;
     }
     
+    /**
+     * Update the automaton to its next state.
+     * Acts like a sliding window of 3 cells where only the right
+     * neighbour of the center psositon is fetched from the copy of
+     * state array. Now, do a sliding window motion by an increment of 1
+     * So,after moving center position by one, we shift by one.
+     * Meaning, center becomes new left, and right becomes new center.
+     */
+    public void update()
+    {
+        // Build the new state in a separate array.
+        int[] nextState = new int[state.length];
+        //Create the final extended array with ghost cell.(is the next step)
+        final int[] copy = Arrays.copyOf(state, state.length + 1);
+        // Naively update the state of each cell
+        // based on the state of its two neighbors.
+        int left = 0;
+        //read from our new safe array
+        //Though I believe reading from the OG state works?
+        int center = copy[0];
+        /**
+         * The conditon is state.length, not copy.length because we want 
+         * to read only from indexes available and not extra ghost cell 
+         * from the copy one, I think.
+         */
+        for (int i=0; i<state.length; i++){
+            //base on the copy, because it has that i+1 cell that state has not.
+            int right = copy[i+1];
+            //calculate to find either 1 or 0.
+            nextState[i] = calculateNextState(left, center, right);
+            //sliding window described above.
+            left = center;
+            center = right;
+        }
+        /**
+         * We put all the newly found values of the nextState array into 
+         * state in order to print it when doing whether run or step method.
+         */
+        state = nextState;
+    }
     
     /**
      * Update the automaton to its next state.
      */
-    public void update()
+    public void updateOG3()
     {
         // Build the new state in a separate array.
         int[] nextState = new int[state.length];
@@ -154,6 +201,5 @@ public class Automaton
         Arrays.fill(state, 0);
         // Seed the automaton with a single 'on' cell.
         state[numberOfCells / 2] = 1;
-        state[0] = 1;
     }
 }
